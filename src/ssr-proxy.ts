79943439588c16b911abcefcc66b972519b32732
Marek Olszewski <marek.olszewski@concent.no>
2023-05-16 11:49:41 +0200

    wip

--- src/ssr-proxy.ts
+++ src/ssr-proxy.ts
@@ -523,6 +523,8 @@ export class SsrProxy {
 
             logger.debug('SSR: Connected');
 
+            // await this.waitTillHTMLRendered(page);
+
             // Serialized text of page DOM
             const text = await page.content();
 
@@ -539,6 +541,36 @@ export class SsrProxy {
             return { ttRenderMs, error };
         }
     }
+    private async waitTillHTMLRendered(page: puppeteer.Page, timeout = 30000) {
+        const checkDurationMsecs = 1000;
+        const maxChecks = timeout / checkDurationMsecs;
+        let lastHTMLSize = 0;
+        let checkCounts = 1;
+        let countStableSizeIterations = 0;
+        const minStableSizeIterations = 3;
+      
+        while(checkCounts++ <= maxChecks){
+          let html = await page.content();
+          let currentHTMLSize = html.length; 
+      
+          let bodyHTMLSize = await page.evaluate(() => document.body.innerHTML.length);
+      
+          console.log('last: ', lastHTMLSize, ' <> curr: ', currentHTMLSize, " body html size: ", bodyHTMLSize);
+      
+          if(lastHTMLSize != 0 && currentHTMLSize == lastHTMLSize) 
+            countStableSizeIterations++;
+          else 
+            countStableSizeIterations = 0; //reset the counter
+      
+          if(countStableSizeIterations >= minStableSizeIterations) {
+            console.log("Page rendered fully..");
+            break;
+          }
+      
+          lastHTMLSize = currentHTMLSize;
+          await page.waitForTimeout(checkDurationMsecs);
+        }  
+      };
 
     private getContentType(path: string) {
         const isHtml = () => /\.html$/.test(path) || !/\./.test(path)
--- test/index.ts
+++ test/index.ts
@@ -1,10 +1,31 @@
-import { LogLevel, SsrProxy, SsrProxyConfig } from 'ssr-proxy-js';
+import { LogLevel, ProxyType, SsrProxy, SsrProxyConfig } from '../src';
 
 const config: SsrProxyConfig = {
-    port: 8081,
+    port: 8089,
     hostname: '0.0.0.0',
-    targetRoute: 'localhost:3000',
-    log: { level: LogLevel.Info },
+    targetRoute: '79element.pl',
+    log: { level: LogLevel.Debug },
+    proxyOrder: [ProxyType.SsrProxy],
+    isBot: (method, url, headers) => true,
+    ssr: {
+        // shouldUse: params => true,
+        shouldUse: params => (/\.html$/.test(params.targetUrl) || !/\./.test(params.targetUrl.replace('79element.pl', ''))),
+        browserConfig: {
+            // headless: false,
+            // devtools: true,
+            args: [
+                '--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage',
+                '--disable-web-security',
+                '--disable-features=IsolateOrigins',
+                '--disable-site-isolation-trials'
+            ]
+        },
+        allowedResources:  ['document', 'script', 'xhr', 'fetch', 'stylesheet'],
+        waitUntil: 'networkidle2'
+    },
+    cache: {
+        shouldUse: _ => false,
+    }
 };
 
 const ssrProxy = new SsrProxy(config);
